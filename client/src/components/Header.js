6874a674c146c55d6b3d75fbff5f78ac5e4d89b9
Suman Rakshit <110285767+SumanRakshit707@users.noreply.github.com>
2025-06-01 02:23:46 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo-circle">
+        <img src="/logo.png" alt="Logo" className="logo-img" />
+      </div>
+      <h1 className="title">Digital Signature Creator</h1>
+    </header>
+  );
+}
+
+export default Header;
 
