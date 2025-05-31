427b225855888f829d60e20731b9d3a6243dffa0
Suman Rakshit <110285767+SumanRakshit707@users.noreply.github.com>
2025-06-01 02:25:03 +0530

    Update Home.js

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <main className="home">
+      <button className="btn" onClick={() => navigate('/draw')}>Draw Your Signature</button>
+      <button className="btn" onClick={() => navigate('/upload')}>Upload Your Signature</button>
+    </main>
+  );
+}
+
+export default Home;
 
